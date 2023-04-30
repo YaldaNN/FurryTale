844ab24f34d8fe17bcbb55c6a7d0ed90044f2704
Angie Oh <>
2023-04-29 21:24:47 -0700

    Updated "createFurryTaleData.js" file to have two different account data.

--- CPSC-5240-4240/MongooseDB/createDB/createFurryTaleData.js
+++ CPSC-5240-4240/MongooseDB/createDB/createFurryTaleData.js
@@ -1,15 +1,16 @@
 db = db.getSiblingDB('FurryTale')
-
-db.createCollection('accounts')
+//db.createCollection('accounts')
 accountsCollection = db.getCollection("accounts")
 accountsCollection.remove({})
-accountsCollection.insert(
+accountsCollection.insertOne(
 {
 	  accountId: "1",
 	  userId: "1",
 	  accountType: 1,
 	  payment: 0.0,
-},
+}
+)
+accountsCollection.insertOne(
 {
 	accountId: "2",
 	userId: "2",
