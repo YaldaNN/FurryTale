479ad7e1a1dfea58084d8c883e7e3d3a582514dc
Angie Oh <>
2023-04-29 23:44:17 -0700

    Added Achievement data to "createFurryTaleData.js"

--- CPSC-5240-4240/MongooseDB/createDB/createFurryTaleData.js
+++ CPSC-5240-4240/MongooseDB/createDB/createFurryTaleData.js
@@ -1,3 +1,4 @@
+// Creating Collection for ACCOUNTS
 db = db.getSiblingDB('FurryTale')
 //db.createCollection('accounts')
 accountsCollection = db.getCollection("accounts")
@@ -18,3 +19,34 @@ accountsCollection.insertOne(
 	payment: 0.0,
 }
 )
+
+// Creating Collection for ACHIEVEMENTS
+db.createCollection('achievements')
+achievementsCollection = db.getCollection("achievements")
+achievementsCollection.remove({})
+achievementsCollection.insert(
+{
+	acheivementId: "1",
+	userId: "1",
+	achievementType: 2,
+	achievementDetail: "WON 1st PLACE",
+}
+)
+achievementsCollection.insert(
+	{
+		acheivementId: "2",
+		userId: "1",
+		achievementType: 1,
+		achievementDetail: "Went to furry competition",
+	}
+	)
+	
+// db.createCollection('users')
+// usersCollection = db.getCollection("users")
+// usersCollection.remove({})
+// usersCollection.insert(
+// {
+// 	userId: "1",
+	
+// }
+// )
