e98c52042b20c199b9207c69cc6812a88bf60a79
Angie Oh <>
2023-04-29 17:37:13 -0700

    New DB called "createFurryTaleData.js" added under MongooseDB folder.

---
+++ CPSC-5240-4240/MongooseDB/createDB/createFurryTaleData.js
@@ -0,0 +1,19 @@
+db = db.getSiblingDB('FurryTale')
+
+db.createCollection('accounts')
+accountsCollection = db.getCollection("accounts")
+accountsCollection.remove({})
+accountsCollection.insert(
+{
+	  accountId: 1,
+	  userId: 1,
+	  accountType: 1,
+	  payment: 0.0,
+},
+{
+	accountId: 2,
+	userId: 2,
+	accountType: 1,
+	payment: 0.0,
+}
+)
