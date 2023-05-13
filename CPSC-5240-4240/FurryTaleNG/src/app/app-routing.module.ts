982097a7b6bd1e4570bb5cd18a049d9176fc868c
YaldaNN <ynafisinia@seattleu.edu>
2023-05-13 12:45:50 -0700

    Created the createProfile component under FurryTaleNG/app folder

--- CPSC-5240-4240/FurryTaleNG/src/app/app-routing.module.ts
+++ CPSC-5240-4240/FurryTaleNG/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateProfileComponent } from './create-profile/create-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'createProfile', component: CreateProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
