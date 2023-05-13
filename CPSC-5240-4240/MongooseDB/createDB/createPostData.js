fc52fd8fd928fdd600094fc63d0c3509565f4307
aniquaTabassum <aniqua.tabassum337@gmail.com>
2023-05-13 11:41:49 -0700

    Added Angular App

--- CPSC-5240-4240/MongooseDB/createDB/createPostData.js
+++ CPSC-5240-4240/MongooseDB/createDB/createPostData.js
@@ -3,14 +3,15 @@ db = db.getSiblingDB('FurryTale')
 //db.createCollection('posts')
 postsCollection = db.getCollection("posts")
 postsCollection.remove({})
-postsCollection.insertOne(
-{
-    postId : "1",
-    userId : "2",
-    postType : 1,
-    image : "Image Location",
-    caption: "Homeowner can be pronounced as hoMEOWner. Good luck pronouncing it correct again",
-    paws : []
-}
-)
+
+postsCollection.insertOne( {
+   
+    postId: "24ab0322f899e7a6389f0857c43b5b1e",
+    userId: "2c78a513a28f2bf1c680b505955a7bad",
+    postType: 1,
+    image: "https://images.unsplash.com/photo-1545328951-d483a3667ed0?ixlib=rb-1.2.1\u0026ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aGFwcHklMjBkb2d8ZW58MHx8MHx8\u0026auto=format\u0026fit=crop\u0026w=900\u0026q=60",
+    caption: "Meow meow meow",
+    paws: []
+  
+})
 
