9fab17cb33c78c61d6d6c3b370946039200fdcfa
aniquaTabassum <aniqua.tabassum337@gmail.com>
2023-05-13 13:15:54 -0700

    Added Home Component

--- CPSC-5240-4240/FurryTaleNG/src/app/app-routing.module.ts
+++ CPSC-5240-4240/FurryTaleNG/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateProfileComponent } from './create-profile/create-profile.component';
-import { OpportunitiesComponent } from './opportunities/opportunities.component'
+import { OpportunitiesComponent } from './opportunities/opportunities.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-  { path: 'createProfile', component: CreateProfileComponent}
-  { path: 'opportunities', component: OpportunitiesComponent}
+  { path: 'createProfile', component: CreateProfileComponent},
+  { path: 'opportunities', component: OpportunitiesComponent},
+  { path: 'home', component: HomeComponent}
 
-];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- CPSC-5240-4240/FurryTaleNG/src/app/app.module.ts
+++ CPSC-5240-4240/FurryTaleNG/src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateProfileComponent } from './create-profile/create-profile.component';
 import { OpportunitiesComponent } from './opportunities/opportunities.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateProfileComponent
-    OpportunitiesComponent
+    CreateProfileComponent,
+    OpportunitiesComponent,
+    CreateProfileComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
