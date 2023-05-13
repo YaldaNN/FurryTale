8c8605b4e916e3b0a3261eae621ad64ba368b728
Angie Oh <>
2023-05-13 13:12:22 -0700

    Added Opportunities Routings to app-routing.module.ts & app.module.ts

--- CPSC-5240-4240/FurryTaleNG/src/app/app-routing.module.ts
+++ CPSC-5240-4240/FurryTaleNG/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateProfileComponent } from './create-profile/create-profile.component';
+import { OpportunitiesComponent } from './opportunities/opportunities.component'
+
 
 const routes: Routes = [
   { path: 'createProfile', component: CreateProfileComponent}
+  { path: 'opportunities', component: OpportunitiesComponent}
+
 ];
 
 @NgModule({
--- CPSC-5240-4240/FurryTaleNG/src/app/app.module.ts
+++ CPSC-5240-4240/FurryTaleNG/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateProfileComponent } from './create-profile/create-profile.component';
+import { OpportunitiesComponent } from './opportunities/opportunities.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateProfileComponent
+    OpportunitiesComponent
   ],
   imports: [
     BrowserModule,
