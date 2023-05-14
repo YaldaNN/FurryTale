a7b96e4ab79936bcac2bcce32bf792f6e769960b
Angie Oh <>
2023-05-13 21:52:14 -0700

    Added post data in createPostData.js

--- CPSC-5240-4240/MongooseDB/createDB/createPostData.js
+++ CPSC-5240-4240/MongooseDB/createDB/createPostData.js
@@ -15,3 +15,14 @@ postsCollection.insertOne( {
   
 })
 
+postsCollection.insertOne( {
+   
+    postId: "24ab0322f899e7a6389f0857c43b5b1f",
+    userId: "2c78a513a28f2bf1c680b505955a8bad",
+    postType: 1,
+    image: "https://images.unsplash.com/photo-1545328951-d483a3667ed0?ixlib=rb-1.2.1\u0026ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aGFwcHklMjBkb2d8ZW58MHx8MHx8\u0026auto=format\u0026fit=crop\u0026w=900\u0026q=60",
+    caption: "Woof Woof Woof",
+    paws: []
+  
+})
+
