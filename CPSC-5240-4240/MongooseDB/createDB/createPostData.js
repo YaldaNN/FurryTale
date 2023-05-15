290e90459f3eb45e0fc905d074f230b18ad32911
Angie Oh <>
2023-05-15 15:49:49 -0700

    Added more user data and post data under createDB.

--- CPSC-5240-4240/MongooseDB/createDB/createPostData.js
+++ CPSC-5240-4240/MongooseDB/createDB/createPostData.js
@@ -28,12 +28,44 @@ postsCollection.insertOne( {
 
 postsCollection.insertOne( {
    
+    postId: "24ab0322f899e7a6389f0857c43b5b2b",
+    userId: "2c78a513a28f2bf1c680b505955a7bad",
+    postType: 2,
+    image: "https://images.unsplash.com/photo-1545328951-d483a3667ed0?ixlib=rb-1.2.1\u0026ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aGFwcHklMjBkb2d8ZW58MHx8MHx8\u0026auto=format\u0026fit=crop\u0026w=900\u0026q=60",
+    caption: "Come and join!",
+    paws: []
+  
+})
+
+postsCollection.insertOne( {
+   
     postId: "24ab0322f899e7a6389f0857c43b5b1f",
-    userId: "2c78a513a28f2bf1c680b505955a8bad",
-    postType: 1,
+    userId: "3c78a513a28f2bf1c680b505955a7bad",
+    postType: 2,
     image: "https://images.unsplash.com/photo-1545328951-d483a3667ed0?ixlib=rb-1.2.1\u0026ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aGFwcHklMjBkb2d8ZW58MHx8MHx8\u0026auto=format\u0026fit=crop\u0026w=900\u0026q=60",
     caption: "Woof Woof Woof",
     paws: []
   
 })
 
+postsCollection.insertOne( {
+   
+    postId: "24ab0322f899e7a6389f0857c43b5b1f",
+    userId: "3c78a513a28f2bf1c680b505955a7bad",
+    postType: 3,
+    image: "https://images.unsplash.com/photo-1545328951-d483a3667ed0?ixlib=rb-1.2.1\u0026ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aGFwcHklMjBkb2d8ZW58MHx8MHx8\u0026auto=format\u0026fit=crop\u0026w=900\u0026q=60",
+    caption: "competition",
+    paws: []
+  
+})
+
+postsCollection.insertOne( {
+   
+    postId: "24ab0322f899e7a6389f0857c43b5b1f",
+    userId: "3c78a513a28f2bf1c680b505955a7bad",
+    postType: 4,
+    image: "https://images.unsplash.com/photo-1545328951-d483a3667ed0?ixlib=rb-1.2.1\u0026ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aGFwcHklMjBkb2d8ZW58MHx8MHx8\u0026auto=format\u0026fit=crop\u0026w=900\u0026q=60",
+    caption: "Adopt this cute dog!",
+    paws: []
+  
+})
--- CPSC-5240-4240/MongooseDB/createDB/createUser.js
+++ CPSC-5240-4240/MongooseDB/createDB/createUser.js
@@ -4,9 +4,7 @@ db = db.getSiblingDB('FurryTale')
 usersCollection = db.getCollection("users")
 usersCollection.remove({})
 
-usersCollection.insertOne({
-    
-     
+usersCollection.insertOne({   
         userId: "2c78a513a28f2bf1c680b505955a7bad",
         userName: "Oreo",
         userPassword: "password",
@@ -18,6 +16,35 @@ usersCollection.insertOne({
         verified: false,
         verificationBadgeId: "",
         profilePic : "https://wallpaper.dog/large/5439024.jpg",
-        email : "meow@pet.com"
-    
+        email : "Oreo@pet.com"
+})
+
+usersCollection.insertOne({   
+        userId: "3c78a513a28f2bf1c680b505955a7bad",
+        userName: "Coco",
+        userPassword: "password",
+        accountId: "d33e32beea6efc9d43205fa91e732031",
+        tailers: [],
+        tailee: [],
+        about: "Hello I'm Coco",
+        openToWork: false,
+        verified: false,
+        verificationBadgeId: "",
+        profilePic : "https://images.pexels.com/photos/2607544/pexels-photo-2607544.jpeg?auto=compress&cs=tinysrgb&w=600",
+        email : "coco@pet.com"
+})
+
+usersCollection.insertOne({   
+        userId: "4c78a513a28f2bf1c680b505955a7bad",
+        userName: "Seattle Animal Shelter",
+        userPassword: "password",
+        accountId: "d43e32beea6efc9d43205fa91e732031",
+        tailers: [],
+        tailee: [],
+        about: "Come visit Seattle Animal Shelter",
+        openToWork: false,
+        verified: true,
+        verificationBadgeId: "123",
+        profilePic : "https://dl5zpyw5k3jeb.cloudfront.net/organization-photos/39343/1/?bust=1613869306",
+        email : "seattleAnimalShelter@pet.com"
 })
